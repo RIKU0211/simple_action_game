6787b0101c3809fea1a67bcc62a4883f14722f83
RIKU0211 <x.blade.riku0211@gmail.com>
2013-12-08 23:05:09 +0900

    scroll jump update

--- js/test.js
+++ js/test.js
@@ -7,18 +7,80 @@ var Ground = function(filename) {
 
 };
 
-var Background = function(filename){
-  this.sprite = new Lib.Sprite(filename, 256, 256);
+/*
+ *  プレイヤー
+ */
+var Player = function(filename){
+  this.sprite = new Lib.Sprite(filename, 96, 192, 32, 48);
+  var manager = Lib.InputManager.getInstance();
+  var is_jump;
+  
+  this.initialize = function(){
+    this.sprite.x = 50;
+    this.sprite.y = 252;
 
-  this.update = function(){
-    this.sprite.x -= 10;
+    is_jump = false;
   }
 
+  this.update = function(){
+
+    var position = new Lib.Vec2(250, 250);
+    var timer = 0;
+
+    canvas.addEventListener("keydown", function(e) {
+      if(manager.getKey("SPACE") == e.keyCode) {
+        // ジャンプ処理
+        is_jump = true;
+
+        if(is_jump) {
+          position.y = Lib.ActionUtility.jumpToVertical(position.y, timer);
+        }
+
+        timer += 0.05;
+
+        if(position.y >= 300) {
+          timer = 0;
+          is_jump = false;
+        }
+      }
+    });
+
+  }
+
+  this.draw = function(){
+    this.sprite.animation(context, 2);
+  }
+};
+
+/*
+ *  背景
+ */
+var Background = function(filename, scroll){
+  this.sprite = new Lib.Sprite(filename, 256, 256);
+  if (scroll == 1) {
+    this.sprite.x += 800;
+  }
+
+  // 更新
+  this.update = function(){
+    // 画像のスクロールスピード調整
+    this.sprite.x -= 5;
+
+    // スクロール処理
+    if(this.sprite.x == -800){
+      this.sprite.x += 1600;
+    }
+  }
+
+  // 描画
   this.draw = function(context){
     this.sprite.draw(context);
   }
 };
 
+/*
+ *  落とし穴
+ */
 var DropFall = function(filename){
   this.sprite = new Lib.Sprite(filename, 36, 36);
   
@@ -31,6 +93,9 @@ DropFall.prototype = {
   }
 };
 
+/*
+ *  ブロック
+ */
 var Block = function(filename){
   this.sprite = new Lib.Sprite(filename, 36, 36);
 };
