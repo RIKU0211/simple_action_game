8b341019809fe2831541be88e0545da4b5facdb9
RIKU0211 <x.blade.riku0211@gmail.com>
2013-12-10 00:13:49 +0900

    12/10 update

--- js/test.js
+++ js/test.js
@@ -13,44 +13,31 @@ var Ground = function(filename) {
   };
 };
 
-<<<<<<< HEAD
+
 /*
  *  プレイヤー
  */
 var Player = function(filename){
   this.sprite = new Lib.Sprite(filename, 96, 192, 32, 48);
   var manager = Lib.InputManager.getInstance();
-  var is_jump;
+  var position = new Lib.Vec2(250, 250);
+  var timer = 0;
   
   this.initialize = function(){
     this.sprite.x = 50;
     this.sprite.y = 252;
-
-    is_jump = false;
   }
 
   this.update = function(){
 
-    var position = new Lib.Vec2(250, 250);
-    var timer = 0;
-
     canvas.addEventListener("keydown", function(e) {
-      if(manager.getKey("SPACE") == e.keyCode) {
+      if(manager.getKey("UP") == e.keyCode) {
         // ジャンプ処理
-        is_jump = true;
-
-        if(is_jump) {
-          position.y = Lib.ActionUtility.jumpToVertical(position.y, timer);
-        }
-
-        timer += 0.05;
-
-        if(position.y >= 300) {
-          timer = 0;
-          is_jump = false;
-        }
+        position.y = Lib.ActionUtility.jumpToVertical(position.y, timer);
       }
     });
+
+    timer += 0.5;
 
   }
 
@@ -79,74 +66,31 @@ var Background = function(filename, scroll){
     }
   }
 
-  // 描画
-  this.draw = function(context){
-=======
-/**
- * background class
- */
-var Background = function(filename){
-  this.sprite = new Lib.Sprite(filename, 256, 256);
-  
-  /**
-   * update background
-   */
-  this.update = function() {
-    this.sprite.x -= 10;
-  };
-  
   /**
    * draw background
    */
   this.draw = function(context) {
->>>>>>> d6b2d38df503a6469c618a9acee67f07345474e1
     this.sprite.draw(context);
   };
 };
 
-<<<<<<< HEAD
-/*
- *  落とし穴
- */
-var DropFall = function(filename){
-=======
-/**
- * Drop Fall Class
- */
-var DropFall = function(filename) {
->>>>>>> d6b2d38df503a6469c618a9acee67f07345474e1
-  this.sprite = new Lib.Sprite(filename, 36, 36);
-  
-};
+var Sound = function(){
+  var manager = Lib.AudioManager.getInstance();
 
-/**
- * Drop Fall's prototype
- */
-DropFall.prototype = {
-  update: function() {
-    this.sprite.draw(context);
+  this.initialize = function(){
+    manager.add("snd/test.mp3", "test");
+    manager.setLoop(true);
+  }
+
+  this.play = function(){
+    manager.play("test");
+
+    canvas.addEventListener("keydown", function(e) {
+      if(manager.getKey("SPACE") == e.keyCode) {
+        // 一時停止
+        manager.pause("test");
+      }
+    });
   }
 };
 
-<<<<<<< HEAD
-/*
- *  ブロック
- */
-var Block = function(filename){
-=======
-/**
- * Block Class
- */
-var Block = function(filename) 
->>>>>>> d6b2d38df503a6469c618a9acee67f07345474e1
-  this.sprite = new Lib.Sprite(filename, 36, 36);
-};
-
-/**
- * Block's prototype
- */
-Block.prototype = {
-  update: function() 
-    this.sprite.draw(context);
-  }
-};
